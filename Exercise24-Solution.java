ebd7954bec91ad61df10488bb149e272f420b800
PauloRaphael <55895385+PauloRaphael@users.noreply.github.com>
2022-11-04 12:31:50 -0300

    Exercise 24
    You are given n lines. In each line there are zero or more integers. You need to answer a few queries where you need to tell the number located in y position of x line.
    
    Take your input from System.in.

---
+++ Exercise24-Solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        List<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        Scanner scan = new Scanner(System.in);
+        
+        int n = scan.nextInt();
+        
+        for(int i = 0; i < n; i++) {
+            
+            List<Integer> subList = new ArrayList<Integer>();
+            
+            subList.add(6);
+            
+            int m = scan.nextInt();
+            for(int j = 0; j < m; j++) {
+        
+               
+        
+            }     
+            
+       }
+    }
+}
