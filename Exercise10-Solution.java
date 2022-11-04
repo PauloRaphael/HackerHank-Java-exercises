9de86a326bad6fae73cbd26ece264c21515ad9b4
PauloRaphael <55895385+PauloRaphael@users.noreply.github.com>
2022-11-04 03:27:41 -0300

    Exercise 10
    You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth B and height H. You should read the variables from the standard input.
    
    If B <= 0 or H <=0 , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.

--- Exercise10-Solution.java
+++ Exercise10-Solution.java
@@ -1,17 +1,28 @@
 import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
+  
+ static int B = 1;
+ static int H = 3;
+ static boolean flag =  B > 0 && H > 0;
+ 
+ static {
+     if(!flag) {
+             System.out.println("java.lang.Exception: Breadth and height must be positive");
+         }       
+     }
+ 
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
 
-    public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
-        
-        int i = 1;
-        
-        while(scan.hasNext()) {
-        String s = scan.nextLine();
-        System.out.println(i + " " + s);
-        i++;
-        }
-    }
-}
+}//end of class
+
